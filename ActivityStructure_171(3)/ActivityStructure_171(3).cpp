d9ecb009d290bf7a2ed744b4cf405e3fd4cd99d1
arifrahmadi23 <muhammad231arif@gmail.com>
2023-04-03 15:29:20 +0700

    membuat inputan untuk data mhs, dan membuat perulangan

---
+++ ActivityStructure_171(3)/ActivityStructure_171(3).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+struct AlamatDetail
+{
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa {
+	char nim[12];
+	char nama[20];
+	AlamatDetail alamat;
+	int umur;
+};
+
+int main() {
+
+	Mahasiswa mhs[3];
+
+	for (int = 0; i < 3; i++)
+	{
+
+	}
+}
